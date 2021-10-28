f366735f0ce0c660db21e29a005c308d58381738
fawaz bayureksa <fawwazbayureksa@gmail.com>
2021-10-28 16:45:45 +0800

    commit lagi 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,24 @@ function Header() {
                 <img src="/images/logo.svg" Alt="Logo Tesla"/>
             </a>
             <Menu>
-
+                <p>
+                    <a href="#">Model S</a>
+                </p>
+                <p>
+                    <a href="#">Model 3</a>
+                </p>
+                <p>
+                    <a href="#">Model X</a>
+                </p>
+                <p>
+                    <a href="#">Model Y</a>
+                </p>
+                <p>
+                    <a href="#">Solar Roof</a>
+                </p>
+                <p>
+                    <a href="#">Solar Panels</a>
+                </p>
             </Menu>
         </Container>
     )
@@ -18,10 +35,18 @@ export default Header
 
 const Container = styled.div`
     min-height:60px;
-    
-    // position:fixed; Agar navbar selalu diatas
-
+    position:fixed; //Agar navbar selalu diatas
+    display:flex;
+    align-items:center;
+    padding:0 20px;
+    justify-content:space-between;
 `
 const Menu = styled.div`
+    display:flex;
+    align-items:center;
 
+    p{
+        font-weight:600;
+        text-transform:uppercase; // Membuat semua huruf menjadi Kapital
+    }
 `
